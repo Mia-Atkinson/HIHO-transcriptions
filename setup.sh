979b4a247eae184d57777c1536c196ceb0e9a208
Mia Neil <mia.e.atkinson@gmail.com>
2022-02-09 14:25:16 -0500

    automation updates

--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+export PATH=/usr/local/bin:$PATH
+source ~/.bash_profile
+
 echo "Job Name:"
 read JOB_NAME
+# Group\ Containers/
 export PODCAST_PATH="/Users/mianeil/Library/Group Containers/243LU875E5.groups.com.apple.podcasts/Library/Cache"
+
+cd "$PODCAST_PATH"
 export LONG_MP3=$(ls *.mp3)
-if (mv $PODCAST_PATH/$LONG_MP3 /Users/mianeil/Desktop/HIHO/Audio/$JOB_NAME.mp3)
+
+if (mv $LONG_MP3 /Users/mianeil/Desktop/HIHO/Audio/$JOB_NAME.mp3)
 then
-  echo "Moving from podcasts to working directory"
+  echo "Moving audio from podcasts to working directory"
+  cd ~/Desktop/HIHO
+  aws s3 cp Audio/$JOB_NAME.mp3 s3://hiho-transcription/Audio_Input/$JOB_NAME.mp3
 fi
